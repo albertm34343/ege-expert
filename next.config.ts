2fd25bb91a07b26fbe8923a953fedcde8809a549
albertm34343 <malbert333j@mail.ru>
2026-05-24 00:48:27 +0300

    fix: add server

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'проверка-сочиненийегэ.рф',
+        'www.проверка-сочиненийегэ.рф',
+        'xn----7sbbbqf0afdv1aeezab0a.xn--p1ai',
+        'localhost:3000',
+      ],
+    },
+  },
 };
 
 export default nextConfig;
