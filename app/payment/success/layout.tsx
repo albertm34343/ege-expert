fa58ba4aabd0c6807a4846a988b5b82239869f13
albertm34343 <malbert333j@mail.ru>
2026-05-26 01:39:43 +0300

    fix: css

--- app/payment/success/layout.tsx
+++ app/payment/success/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
-import "./globals.css";
+import "../../globals.css";
 import MouseGlow from "../../components/MouseGlow";
 const inter = Inter({
   subsets: ["latin", "cyrillic"],
